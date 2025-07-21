cbe3bf3cdaf905b980a54f4edcd0d4bc19991942
Arundhat1 <149229660+Arundhat1@users.noreply.github.com>
2025-07-21 21:18:40 +0530

    Update check_comp472A.py

--- check_comp472A.py
+++ check_comp472A.py
@@ -9,3 +9,11 @@ def check_comp(s):
                 x = x + 1
 
     return comp
+def Composit_add():
+    n = int(input())
+    for num in range(2,n):
+        num1 = num
+        num2 = (n - num)
+        if check_comp(num1) == 'True' and check_comp(num2) == 'True':
+            print(num1 , num2)
+            break
